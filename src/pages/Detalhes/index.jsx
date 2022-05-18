2479576a87b583783c0a99504ec2a6d7a79a9655
MISAEL GUILHARDES DE FREITAS <mgfreitas@brasilseg.com.br>
2022-05-18 18:02:27 -0300

    Criando paginas da aplicação

---
+++ src/pages/Detalhes/index.jsx
@@ -0,0 +1,36 @@
+import { useCallback, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { FilmeDetalhe } from '../../components/FilmeDetalhe'
+import { movieApi, defaultParams } from '../../services/api'
+import styles from './styles.module.css'
+export const Detalhes = () => {
+    const { prFilmeId } = useParams()
+    const [filme, setFilme] = useState({})
+
+    const getFilme = useCallback(async () => {
+        const { status, data } = await movieApi.get(`/movie/${prFilmeId}?`, {
+            params: { ...defaultParams },
+        })
+        if (status === 200) {
+            console.log(data)
+            setFilme(data)
+        }
+    }, [prFilmeId])
+
+    useEffect(() => {
+        getFilme()
+    }, [getFilme])
+
+    return (
+        <div
+            className={styles.detalhesContainer}
+            style={{
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                backgroundImage: `url(https://image.tmdb.org/t/p/original${filme.backdrop_path})`,
+            }}
+        >
+            <FilmeDetalhe filme={filme} />
+        </div>
+    )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,59 @@
+import { useCallback, useEffect, useRef, useState } from 'react'
+import { movieApi, defaultParams } from '../../services/api'
+import { FilmeLista } from '../../components/FIlmeLista'
+import styles from './styles.module.css'
+
+export const Home = () => {
+    const [lsFilme, setLsFilme] = useState([])
+    const [nrPagina, setNrPagina] = useState(1)
+    const loadMoreRef = useRef(null)
+
+    const getListaFilmes = useCallback(async (nrPaginaBusca) => {
+        const { status, data } = await movieApi.get('/discover/movie?', {
+            params: { ...defaultParams, page: nrPaginaBusca },
+        })
+        if (status === 200) {
+            setLsFilme((old) => [...old, ...data['results']])
+        }
+    }, [])
+
+    useEffect(() => {
+        if (nrPagina > 0) {
+            getListaFilmes(nrPagina)
+        }
+    }, [nrPagina, getListaFilmes])
+
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0,
+        }
+        const observer = new IntersectionObserver((entities) => {
+            const target = entities[0]
+
+            if (target.isIntersecting) {
+                setNrPagina((old) => old + 1)
+            }
+        }, options)
+
+        if (loadMoreRef.current) {
+            observer.observe(loadMoreRef.current)
+        }
+    }, [])
+    return (
+        <div className={styles.homeContainer}>
+            <div className={styles.homeTitleContainer}>
+                <h4>Filmes</h4>
+            </div>
+
+            <div>
+                <FilmeLista lsFilme={lsFilme} />
+            </div>
+
+            <div ref={loadMoreRef}>
+                <FilmeLista lsFilme={[1, 2, 3, 4, 5]} inLoading={true} />
+            </div>
+        </div>
+    )
+}
